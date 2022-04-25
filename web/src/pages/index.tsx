abb5e5baae6b2921f8e5040c4ea5a8d62ee85414
zagob <matheusus1_23@hotmail.com>
2022-04-25 16:53:30 -0300

    Aula 01 - Estrutura base dos projetos

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { getAccessToken, getSession, useUser } from "@auth0/nextjs-auth0";
+import type { GetServerSideProps, NextPage } from "next";
+
+const Home: NextPage = () => {
+  return null;
+};
+
+export default Home;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+  const token = getAccessToken(req, res);
+
+  console.log(token)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
